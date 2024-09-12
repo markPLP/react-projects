75de778d92539431c7d9624e40ae5e0c8506bf30
markPLP <mpclaspinas@gmail.com>
2024-09-12 20:55:44 +0800

    Practice useState, fetch-render API

--- 02-tours/src/App.jsx
+++ 02-tours/src/App.jsx
@@ -1,6 +1,63 @@
+import { useEffect, useState } from 'react';
+import Tours from './Tours';
+import Loading from './Loading';
+
 const url = 'https://www.course-api.com/react-tours-project';
 
 const App = () => {
-  return <h2>Tours Starter</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    // return items that does not match id
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      // console.log(tours);
+      setTours(tours);
+    } catch (error) {
+      console.error(error, 'Failed to fetch data');
+    }
+    // set loading to false after fetching regardless success or not
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // if loading is true return loading component
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button type="button" className="btn" onClick={() => fetchData()}>
+            reload tours
+          </button>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
 };
 export default App;
