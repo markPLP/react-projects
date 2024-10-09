d538a2c548e5e7ef673ac7a7badb8c4a964c59b4
markPLP <mpclaspinas@gmail.com>
2024-10-09 14:36:55 +0800

    Build porfolio site - tailwind css

---
+++ 19-tailwind-portfolio/src/components/About.jsx
@@ -0,0 +1,22 @@
+import aboutImg from '../assets/about.svg';
+import SectionTitle from './SectionTitle';
+const About = () => {
+  return (
+    <section className='bg-white py-10' id='about'>
+      <div className='align-element grid md:grid-cols-2 items-center gap-16'>
+        <img src={aboutImg} className='w-full h-64' />
+        <article>
+          <SectionTitle title='know more about me' />
+          <p className='text-slate-600 mt-8 leading-loose'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint
+            maiores itaque doloribus asperiores dolorum quas voluptates
+            distinctio, porro beatae quam provident libero animi totam,
+            praesentium voluptatem corrupti dignissimos ipsum accusantium!
+          </p>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default About;
