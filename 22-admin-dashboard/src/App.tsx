b96077262e1da91625964c424df5b1c6545d5720
markPLP <mpclaspinas@gmail.com>
2025-02-12 20:38:43 +0800

    create orders and orders components

--- 22-admin-dashboard/src/App.tsx
+++ 22-admin-dashboard/src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import OverviewPage from './pages/OverviewPage';
-import { ProductsPage, SalesPage, UsersPage } from './pages';
+import { OrdersPage, ProductsPage, SalesPage, UsersPage } from './pages';
 import Sidebar from './components/common/Sidebar';
 
 const App = () => {
@@ -17,8 +17,8 @@ const App = () => {
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/users" element={<UsersPage />} />
         <Route path="/sales" element={<SalesPage />} />
-        {/*  <Route path="/orders" element={<OrdersPage />} />
-        <Route path="/analytics" element={<AnalyticsPage />} />
+        <Route path="/orders" element={<OrdersPage />} />
+        {/*   <Route path="/analytics" element={<AnalyticsPage />} />
         <Route path="/settings" element={<SettingsPage />} /> */}
       </Routes>
     </div>
--- 22-admin-dashboard/src/pages/index.ts
+++ 22-admin-dashboard/src/pages/index.ts
@@ -2,3 +2,4 @@ export { default as Overview } from './OverviewPage';
 export { default as ProductsPage } from './ProductsPage';
 export { default as UsersPage } from './UsersPage';
 export { default as SalesPage } from './SalesPage';
+export { default as OrdersPage } from './OrdersPage';
